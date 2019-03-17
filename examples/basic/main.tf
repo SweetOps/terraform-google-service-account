98765ea7c16ff640dd4971bb5199dc28fd8136ff
SweetOps <vladimir.fess@gmail.com>
2019-03-17 23:30:37 +0200

    ci: implement github actions

---
+++ examples/basic/main.tf
@@ -0,0 +1,6 @@
+module "my_awesome_user" {
+  source    = "git::https://github.com/SweetOps/terraform-google-service-account.git"
+  name      = "user"
+  stage     = "production"
+  namespace = "sweetops"
+}
