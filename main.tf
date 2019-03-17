3c0cf54c6315197121d49c27f6932bd813af67b4
SweetOps <vladimir.fess@gmail.com>
2019-03-17 23:24:30 +0200

    refactor: codebase

--- main.tf
+++ main.tf
@@ -1,12 +1,26 @@
-data "google_client_config" "default" {}
+module "label" {
+  source      = "git::https://github.com/SweetOps/terraform-null-label.git?ref=tags/0.6.1"
+  enabled     = "${var.enabled}"
+  namespace   = "${var.namespace}"
+  name        = "${var.name}"
+  stage       = "${var.stage}"
+  environment = "${var.environment}"
+  delimiter   = "${var.delimiter}"
+  attributes  = "${var.attributes}"
+  context     = "${var.context}"
+  tags        = "${var.tags}"
+}
 
 resource "google_service_account" "default" {
-  account_id   = "${var.account_id}"
-  display_name = "${var.display_name}"
-  project      = "${length(var.project) > 0 ? var.project : data.google_client_config.default.project}"
+  count        = "${var.enabled == "true" ? 1 : 0}"
+  account_id   = "${module.label.id}"
+  display_name = "${module.label.id}"
+  project      = "${var.project}"
 }
 
 data "google_iam_policy" "default" {
+  count = "${var.enabled == "true" ? 1 : 0}"
+
   binding {
     role = "${var.role}"
 
@@ -14,9 +28,4 @@ data "google_iam_policy" "default" {
       "serviceAccount:${google_service_account.default.email}",
     ]
   }
-}
-
-resource "google_project_iam_policy" "default" {
-  project     = "${length(var.project) > 0 ? var.project : data.google_client_config.default.project}"
-  policy_data = "${data.google_iam_policy.default.policy_data}"
 }
