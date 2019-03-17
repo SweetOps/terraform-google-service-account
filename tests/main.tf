ba01f63c0ae66fedc9ee6c368055a11d9a50c3de
Vladimir <vladimir.fess@gmail.com>
2019-03-17 23:31:14 +0200

    ci: implement github actions (#4)

---
+++ tests/main.tf
@@ -0,0 +1,25 @@
+module "my_awesome_user" {
+  source    = "../"
+  name      = "user"
+  stage     = "production"
+  namespace = "sweetops"
+}
+
+module "my_awesome_user_1" {
+  source     = "../"
+  name       = "user"
+  attributes = ["1"]
+  stage      = "production"
+  namespace  = "sweetops"
+  enabled    = "false"
+}
+
+module "my_awesome_user_3" {
+  source     = "../"
+  name       = "user"
+  stage      = "production"
+  namespace  = "sweetops"
+  attributes = ["2"]
+  role       = "roles/storage.objectViewer"
+  enabled    = "true"
+}
