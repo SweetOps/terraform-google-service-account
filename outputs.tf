1ec963cb0222cb407fb1516fabb182edc56ac1ca
Vladimir <vladimir.fess@gmail.com>
2019-03-17 23:26:06 +0200

    refactor: codebase (#3)

--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,14 @@
 output "email" {
-  value       = "${google_service_account.default.email}"
+  value       = "${join("", google_service_account.default.*.email)}"
   description = "The e-mail address of the service account."
 }
 
 output "name" {
-  value       = "${google_service_account.default.name}"
+  value       = "${join("", google_service_account.default.*.name)}"
   description = "The fully-qualified name of the service account."
 }
 
 output "unique_id" {
-  value       = "${google_service_account.default.unique_id}"
+  value       = "${join("", google_service_account.default.*.unique_id)}"
   description = "The unique id of the service account."
-}
-
-output "iam_policy_etag" {
-  value       = "${google_project_iam_policy.default.etag}"
-  description = "The etag of the project's IAM policy."
 }
--- variables.tf
+++ variables.tf
@@ -1,14 +1,56 @@
-variable "account_id" {
-  description = "The service account ID."
+variable "namespace" {
+  type        = "string"
+  description = "Namespace, which could be your organization name or abbreviation, e.g. 'eg' or 'cp'"
 }
 
-variable "display_name" {
-  description = "The display name for the service account."
-  default     = "Managed by Terraform"
+variable "environment" {
+  type        = "string"
+  default     = ""
+  description = "Environment, e.g. 'prod', 'staging', 'dev', 'pre-prod', 'UAT'"
+}
+
+variable "stage" {
+  type        = "string"
+  description = "Stage, e.g. 'prod', 'staging', 'dev', OR 'source', 'build', 'test', 'deploy', 'release'"
+}
+
+variable "name" {
+  type        = "string"
+  description = "Solution name, e.g. 'app' or 'jenkins'"
+}
+
+variable "enabled" {
+  type        = "string"
+  default     = "true"
+  description = "Set to false to prevent the module from creating any resources"
+}
+
+variable "delimiter" {
+  type        = "string"
+  default     = "-"
+  description = "Delimiter to be used between `namespace`, `environment`, `stage`, `name` and `attributes`"
+}
+
+variable "attributes" {
+  type        = "list"
+  default     = []
+  description = "Additional attributes (e.g. `1`)"
+}
+
+variable "tags" {
+  type        = "map"
+  default     = {}
+  description = "Additional tags (e.g. `map('BusinessUnit','XYZ')`"
+}
+
+variable "context" {
+  type        = "map"
+  default     = {}
+  description = "Default context to use for passing state between label invocations"
 }
 
 variable "project" {
-  description = "GCE project name"
+  description = "The project in which the resource belongs. If it is not provided, the provider project is used."
   default     = ""
 }
 
