ca3b9129a6982272f2a6233be36a589f9fca447f
SweetOps <vladimir.fess@gmail.com>
2019-08-28 22:17:40 +0300

    upd

--- variables.tf
+++ variables.tf
@@ -51,8 +51,3 @@ variable "project" {
   default     = ""
   description = "The project in which the resource belongs. If it is not provided, the provider project is used."
 }
-
-variable "role" {
-  type        = string
-  description = "The role/permission that will be granted to the members."
-}
