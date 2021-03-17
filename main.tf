0886faa465abbd371cb4df363e5ecd2235fadb01
SweetOps <vladimir.fess@gmail.com>
2021-03-17 16:58:40 +0200

    feat: add iam_member resource and uodate GH actions

--- main.tf
+++ main.tf
@@ -1,9 +1,12 @@
 locals {
   service_account_key_enabled = module.this.enabled && var.create_service_account_key
+  iam_policy_enabled          = module.this.enabled && length(var.roles) > 0
+  iam_policy_count            = module.this.enabled && length(var.roles) > 0 ? length(var.roles) : 0
 }
 
 resource "google_service_account" "default" {
-  count        = module.this.enabled ? 1 : 0
+  count = module.this.enabled ? 1 : 0
+
   account_id   = module.this.id
   display_name = module.this.id
   description  = var.description
@@ -11,7 +14,8 @@ resource "google_service_account" "default" {
 }
 
 resource "google_service_account_key" "default" {
-  count              = local.service_account_key_enabled ? 1 : 0
+  count = local.service_account_key_enabled ? 1 : 0
+
   service_account_id = join("", google_service_account.default.*.name)
   key_algorithm      = var.key_algorithm
   public_key_type    = var.public_key_type
@@ -20,14 +24,10 @@ resource "google_service_account_key" "default" {
   keepers            = var.keepers
 }
 
-data "google_iam_policy" "default" {
-  count = module.this.enabled ? 1 : 0
+resource "google_project_iam_member" "default" {
+  count = local.iam_policy_count
 
-  dynamic "binding" {
-    for_each = toset(var.roles)
-    content {
-      role    = binding.key
-      members = [format("serviceAccount:%s", join("", google_service_account.default.*.email))]
-    }
-  }
+  project = var.project
+  role    = var.roles[count.index]
+  member  = format("serviceAccount:%s", join("", google_service_account.default.*.email))
 }
